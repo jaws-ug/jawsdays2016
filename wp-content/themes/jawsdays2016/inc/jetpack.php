80c896ffb9cf60d70be1fe671435e97ef720206f
Kazue Igarashi <igarashi@digitalcube.jp>
2015-11-30 19:24:58 +0900

    hastag

--- wp-content/themes/jawsdays2016/inc/jetpack.php
+++ wp-content/themes/jawsdays2016/inc/jetpack.php
@@ -40,6 +40,11 @@ function jawsdays_infinite_scroll_render() {
 	}
 } // end function jawsdays_infinite_scroll_render
 
+/**
+ * Moving Sharing Icons.
+ *
+ * @link http://jetpack.me/2013/06/10/moving-sharing-icons/
+ */
 function jptweak_remove_share() {
 	remove_filter( 'the_content', 'sharing_display',19 );
 	remove_filter( 'the_excerpt', 'sharing_display',19 );
@@ -49,3 +54,46 @@ function jptweak_remove_share() {
 }
  
 add_action( 'loop_start', 'jptweak_remove_share' );
+
+/*
+ * パブリサイズ共有でサフィックス追加
+ * 参考 http://blog.tenshinbo.net/2013/12/12/p-57/
+ * original
+ * $this->default_prefix = Publicize_Util::build_sprintf( array(
+ * 	apply_filters( 'wpas_default_prefix', $this->default_prefix ),
+ * 	'url',
+ * ) );
+ */
+
+add_filter( 'wpas_default_suffix', 'jawsdays_wpas_default_suffix' );
+function jawsdays_wpas_default_suffix( $suffix ) {
+	$suffix = $suffix . " #jawsdays";
+	return $suffix;
+}
+
+add_action( 'publicize_save_meta', 'jawsdays_publicize_save_meta', 10, 4);
+function jawsdays_publicize_save_meta( $submit_post, $post_id, $service_name, $connection ) {
+	$suffix = " #jawsdays";
+	$title  = get_the_title( $post_id );
+	$link   = wp_get_shortlink( $post_id );
+	$publicize_custom_message = get_post_meta( $post_id, '_wpas_mess', true );
+	if ( empty( $publicize_custom_message ) ) {
+		$publicize_custom_message = sprintf(
+			"%s %s %s",
+			$title,
+			$link,
+			$suffix
+		);
+	} else {
+		if ( strpos( $publicize_custom_message, $title ) === false ) {
+			$publicize_custom_message = $publicize_custom_message . $title;
+		}
+		if ( strpos( $publicize_custom_message, $link ) === false ) {
+			$publicize_custom_message = $publicize_custom_message . $link;
+		}
+		if ( strpos( $publicize_custom_message, $suffix ) === false ) {
+			$publicize_custom_message = $publicize_custom_message . $suffix;
+		}
+	}
+	update_post_meta($post_id, '_wpas_mess', $publicize_custom_message);
+}
