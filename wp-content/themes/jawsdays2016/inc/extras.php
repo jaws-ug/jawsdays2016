ce245538bc7f02ea89c2b369abaa926c677b2a9b
Kazue Igarashi <igarashi@digitalcube.jp>
2015-12-08 13:26:34 +0900

    アイキャッチ必須設定

--- wp-content/themes/jawsdays2016/inc/extras.php
+++ wp-content/themes/jawsdays2016/inc/extras.php
@@ -43,3 +43,30 @@ function jawsdays_get_the_archive_title( $title ) {
 	return $title;
 }
 add_filter( 'get_the_archive_title', 'jawsdays_get_the_archive_title' );
+
+/**
+ * WordPressの投稿作成画面で必須項目を作る（空欄ならJavaScriptのアラート）
+ *
+ * @link https://gist.github.com/gatespace/11020994
+ */
+add_action( 'admin_head-post-new.php', 'jawsdays_post_edit_required' ); // 新規投稿画面でフック
+add_action( 'admin_head-post.php', 'jawsdays_post_edit_required' );     // 投稿編集画面でフック
+function jawsdays_post_edit_required() {
+?>
+<script type="text/javascript">
+jQuery(document).ready(function($){
+	if( 'supporter' == $('#post_type').val() || 'speaker' == $('#post_type').val() ){ // サポーターとスピーカー
+		$("#post").submit(function(e){ // 更新あるいは下書き保存を押したとき
+			if( $("#set-post-thumbnail img").length < 1 ) { // アイキャッチ画像
+				alert('アイキャッチ画像を設定してください！');
+				$('.spinner').hide();
+				$('#publish').removeClass('button-primary-disabled');
+				$('#set-post-thumbnail').focus();
+				return false;
+			}
+		});
+	}
+});
+</script>
+<?php
+}
